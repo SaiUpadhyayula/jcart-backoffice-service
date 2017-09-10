35ffa42370e5fdd87bd2ea31fd579be06b2ace8a
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-09-10 11:34:46 +0530

    Added Jacoco code coverage stage to Jenkinsfile

--- src/main/kotlin/com/leantechstacks/jcart/bo/BackofficeServiceApplication.kt
+++ src/main/kotlin/com/leantechstacks/jcart/bo/BackofficeServiceApplication.kt
@@ -2,20 +2,10 @@ package com.leantechstacks.jcart.bo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class BackofficeServiceApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(BackofficeServiceApplication::class.java, *args)
-}
-
-@RestController
-class IndexController
-{
-    @GetMapping("/")
-    fun index() = "backoffice-service-api"
-
 }
